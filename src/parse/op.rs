36acce64ac3832e07d430cca6cc0c8902d47ac65
cloudultima3164 <cloudultima3164@yahoo.com>
2023-12-30 00:14:02 +0900

    Removed commented code

--- src/parse/op.rs
+++ src/parse/op.rs
@@ -17,21 +17,9 @@ pub enum Op {
 impl TryFrom<&str> for Op {
   type Error = ();
   fn try_from(value: &str) -> Result<Self, Self::Error> {
-    serde_json::from_value(serde_json::Value::String(value.to_owned()))
-      .map_err(|_| ())
+    serde_json::from_value(serde_json::Value::String(
+      value.to_owned(),
+    ))
+    .map_err(|_| ())
   }
 }
-
-// pub enum OpParser<'a> {
-//   Include(Parser<'a, IncludeParser<'a>, IncludeArgs>),
-//   Assert(Parser<'a, AssertParser, AssertArgs>),
-//   Assign(Parser<'a, AssignParser, AssignArgs>),
-//   // DbQuery,
-//   Delay(Parser<'a, DelayParser, DelayArgs>),
-//   Exec(Parser<'a, ExecParser, ExecArgs>),
-//   Request(Parser<'a, RequestParser, RequestArgs>),
-// }
-
-// impl<'a> OpParser<'a> {
-
-// }
